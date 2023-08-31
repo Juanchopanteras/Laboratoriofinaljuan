d7cbb020efadb0216202b613d4c9b76a897f49f0
Baco CCB <Baco@ccb.edu.gt>
2023-08-31 10:03:02 -0600

    Create navbar

---
+++ my-site/src/app/components/navbar.js
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./navbar.module.css";
+
+function Navbar() {
+    const [selectedItem, setSelectedItem] = useState("Inicio");
+
+    const handleItemClick = (item) => {
+        setSelectedItem(item);
+    }
+
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ Behavior: "smooth"});
+        }
+
+    
+    };
+    return(
+        <nav className={styles.navbar}> 
+            <div 
+            className={styles.navbarItem + ` ${selectedItem === "Inicio" ? styles.selected : ""}`}
+            onClick={() => handleItemClick("Inicio")}>
+                <a href="#header">Inicio</a>
+            </div>
+            <div 
+            className={styles.navbarItem + ` ${selectedItem === "Habilidades" ? styles.selected : ""}`}
+            onClick={() => handleItemClick("Habilidades")}>
+                <a href= "#info">Habilidades</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
